def5e6316dffde0d4d3263f390bee1fcb0eb089b
Omkar Kanase <129031542+Omkar8284@users.noreply.github.com>
2025-11-04 23:56:15 +0530

    DB Endpoint

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    DB_HOST : 'my3tierdb.cdbmlufgqkjd.ap-south-1.rds.amazonaws.com',
+    DB_HOST : '<DB Endpoint URL>',
     DB_USER : 'admin',
     DB_PWD : 'root123456',
     DB_DATABASE : 'webappdb'
